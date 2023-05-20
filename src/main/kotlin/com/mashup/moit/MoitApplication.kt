58d34740c8d56c5e083826f8cf2970ba43dc8fe2
SeongYunKim <cauboy17@naver.com>
2023-05-21 03:10:22 +0900

    init repository

---
+++ src/main/kotlin/com/mashup/moit/MoitApplication.kt
@@ -0,0 +1,11 @@
+package com.mashup.moit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MoitApplication
+
+fun main(args: Array<String>) {
+    runApplication<MoitApplication>(*args)
+}
---
+++ src/test/kotlin/com/mashup/moit/MoitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mashup.moit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MoitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
