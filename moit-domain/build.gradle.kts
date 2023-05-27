d588c2cf719bcdc09d40f5a40217de9b9f822baa
xonmin <han51361@gmail.com>
2023-05-27 18:55:27 +0900

    feat : connect-postgresql & jasypt

--- moit-domain/build.gradle.kts
+++ moit-domain/build.gradle.kts
@@ -4,9 +4,16 @@ allOpen {
     annotation("javax.persistence.Embeddable")
 }
 
+val postgresqlVersion : String by project.extra
+val jasyptVersion : String by project.extra 
+
 dependencies {
     implementation(project(":moit-common"))
 
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.h2database:h2")
+    // postgresql
+    implementation("org.postgresql:postgresql:$postgresqlVersion")
+    // jasypt
+    api("com.github.ulisesbocchio:jasypt-spring-boot-starter:$jasyptVersion")
 }
---
+++ moit-domain/src/main/kotlin/com/mashup/moit/domain/config/JasyptConfig.kt
@@ -0,0 +1,8 @@
+package com.mashup.moit.domain.config
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig
