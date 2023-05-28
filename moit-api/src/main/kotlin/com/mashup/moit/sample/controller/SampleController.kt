481692be2985cb835b1ea87bd20367668978ceb8
Bobae Kim <tree9295@gmail.com>
2023-05-28 13:47:48 +0900

    style: Editorconfig 적용 (#5)
    * style: added editorconfig
    
    * style: code reformat

--- moit-api/src/main/kotlin/com/mashup/moit/sample/controller/SampleController.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/sample/controller/SampleController.kt
@@ -4,7 +4,12 @@ import com.mashup.moit.common.ApiResponse
 import com.mashup.moit.sample.controller.dto.SampleCreateRequest
 import com.mashup.moit.sample.controller.dto.SampleResponse
 import com.mashup.moit.sample.facade.SampleFacade
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 @RequestMapping("/api/v1/samples")
 @RestController
