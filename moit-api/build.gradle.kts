6971f64857b339aaec28b3c64c8e51655776add1
Bobae Kim <tree9295@gmail.com>
2023-05-28 13:49:07 +0900

    feat: Actuator 추가 (#6)
    * style: added editorconfig
    
    * style: code reformat
    
    * chore: added actuator/prometheus dependencies to api module
    
    * feat: set actuator properties
    
    * feat: added basic http samples for actuator

--- moit-api/build.gradle.kts
+++ moit-api/build.gradle.kts
@@ -15,6 +15,8 @@ dependencies {
     implementation(project(":moit-common"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 }
 
 configure<JibExtension> {
