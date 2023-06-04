6080ef1ab8e9f23aee5cf137f5bbf221d8d7224f
KimDoubleB <tree9295@gmail.com>
2023-06-04 14:56:43 +0900

    feat: Sample User for sample api

---
+++ moit-domain/src/main/kotlin/com/mashup/moit/domain/sample/SampleUser.kt
@@ -0,0 +1,8 @@
+package com.mashup.moit.domain.sample
+
+data class SampleUser(
+    val providerUniqueKey: String,
+    val email: String,
+    val profileImage: String,
+    val nickname: String
+)
