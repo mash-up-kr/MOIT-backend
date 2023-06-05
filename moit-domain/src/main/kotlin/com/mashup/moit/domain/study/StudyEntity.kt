440f973306e0e63b6737a1877e82a977bce0863e
SeongYunKim <cauboy17@naver.com>
2023-06-06 00:07:41 +0900

    chore: set FetchType.LAZY for ManyToOne

--- moit-domain/src/main/kotlin/com/mashup/moit/domain/study/StudyEntity.kt
+++ moit-domain/src/main/kotlin/com/mashup/moit/domain/study/StudyEntity.kt
@@ -39,7 +39,7 @@ class StudyEntity(
     @Column(name = "absence_at", nullable = false)
     val absenceAt: LocalDateTime,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "first_attendance_user_id")
     val firstAttendanceUser: UserEntity?,
 
