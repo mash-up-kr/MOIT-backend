92bf1c7b479a72de6cd21bcb6582abef09446ae5
Bobae Kim <tree9295@gmail.com>
2023-06-14 21:20:03 +0900

    fix(#38): replace NotBlank to NotNull
    Co-authored-by: xonmin <han51361@gmail.com>

--- moit-api/src/main/kotlin/com/mashup/moit/fine/controller/dto/FineConfirmRequest.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/fine/controller/dto/FineConfirmRequest.kt
@@ -1,12 +1,12 @@
 package com.mashup.moit.fine.controller.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 
 @Schema(description = "벌금 송금 평가 RequestBody")
 data class FineEvaluateRequest(
     @Schema(description = "벌금 송금 승인 여부")
-    @field:NotBlank
+    @field:NotNull
     val confirm: Boolean,
 )
