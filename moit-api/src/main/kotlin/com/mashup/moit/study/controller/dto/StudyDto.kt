a7546122a004f2a8443d7a0b44083a0a7aa58d67
KimDoubleB <tree9295@gmail.com>
2023-06-15 21:08:05 +0900

    feat(#38): added getUserAttendanceStatus endpoint, docs

--- moit-api/src/main/kotlin/com/mashup/moit/study/controller/dto/StudyDto.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/study/controller/dto/StudyDto.kt
@@ -1,6 +1,8 @@
 package com.mashup.moit.study.controller.dto
 
+import com.mashup.moit.domain.attendance.AttendanceStatus
 import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDateTime
 
 @Schema(description = "Study 출석 키워드 조회 응답")
 data class StudyAttendanceKeywordResponse(
@@ -22,3 +24,21 @@ data class StudyFirstAttendanceResponse(
     }
 }
 
+@Schema(description = "Study 유저 출석상태 응답")
+data class StudyUserAttendanceStatusResponse(
+    val userId: Long,
+    val nickname: String,
+    val profileImage: Int,
+    val attendanceStatus: AttendanceStatus,
+    val attendanceAt: LocalDateTime
+) {
+    companion object {
+        fun sample() = StudyUserAttendanceStatusResponse(
+            1L,
+            "나는 부자가 될테야",
+            2,
+            AttendanceStatus.ATTENDANCE,
+            LocalDateTime.now()
+        )
+    }
+}
