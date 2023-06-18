0a5d2340d201eb1dbc8b3d6b6fb01e4fcbd3f9a4
SeongYunKim <cauboy17@naver.com>
2023-06-19 00:06:23 +0900

    chore: apply readOnly to UserService Transactional

--- moit-domain/src/main/kotlin/com/mashup/moit/domain/user/UserService.kt
+++ moit-domain/src/main/kotlin/com/mashup/moit/domain/user/UserService.kt
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 class UserService(
     private val userRepository: UserRepository,
 ) {
