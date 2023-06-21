ef2baba4fc1ad1441106b8ef714b292bd2a88c14
KimDoubleB <tree9295@gmail.com>
2023-06-22 02:24:53 +0900

    feat: MoitUser argumentResolver

---
+++ moit-api/src/main/kotlin/com/mashup/moit/security/resolver/MoitUser.kt
@@ -0,0 +1,8 @@
+package com.mashup.moit.security.resolver
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target
+@Schema(hidden = true)
+annotation class MoitUser
---
+++ moit-api/src/main/kotlin/com/mashup/moit/security/resolver/UserInfoArugmentResolver.kt
@@ -0,0 +1,26 @@
+package com.mashup.moit.security.resolver
+
+import com.mashup.moit.security.authentication.JwtAuthentication
+import com.mashup.moit.security.authentication.UserInfo
+import org.springframework.core.MethodParameter
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+// TODO: WebMvcConfigurer 등록 필요
+class UserInfoArugmentResolver : HandlerMethodArgumentResolver {
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(MoitUser::class.java)
+                && parameter.parameter.type == UserInfo::class.java
+    }
+
+    override fun resolveArgument(parameter: MethodParameter, mavContainer: ModelAndViewContainer?, webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?): Any {
+        val jwtAuthentication = SecurityContextHolder.getContext().authentication as JwtAuthentication
+        // TODO: Cast 오류 잡기
+        return jwtAuthentication.principal
+    }
+
+}
