a2a5b4216d7ff36316dec483f34ea3f2a6e58785
KimDoubleB <tree9295@gmail.com>
2023-06-26 17:04:08 +0900

    feat: added JWT secret key property

--- moit-api/src/main/kotlin/com/mashup/moit/config/AuthConfig.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/config/AuthConfig.kt
@@ -1,16 +1,26 @@
 package com.mashup.moit.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.mashup.moit.config.AuthConfig.AuthProperty
 import com.mashup.moit.security.jwt.JwtTokenSupporter
+import org.apache.commons.lang3.StringUtils
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@EnableConfigurationProperties(AuthProperty::class)
 class AuthConfig {
-    
+
     @Bean
-    fun jwtTokenSupporter(mapper: ObjectMapper): JwtTokenSupporter {
-        return JwtTokenSupporter("secret-key-temporary-for-test-123", mapper) // TODO: Secret key 정의 및 주입 필요
+    fun jwtTokenSupporter(authProperty: AuthProperty, mapper: ObjectMapper): JwtTokenSupporter {
+        return JwtTokenSupporter(authProperty.jwtSecretKey, mapper)
     }
-    
+
+    @ConfigurationProperties(prefix = "moit.auth")
+    class AuthProperty {
+        val jwtSecretKey: String = StringUtils.EMPTY
+    }
 }
+
