b932b938b5dbc9b33c5a0804fa41e59e0cc39545
KimDoubleB <tree9295@gmail.com>
2023-06-27 23:19:44 +0900

    feat: AuthProperty to data class, add primary constructor

--- moit-api/src/main/kotlin/com/mashup/moit/config/AuthConfig.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/config/AuthConfig.kt
@@ -3,7 +3,6 @@ package com.mashup.moit.config
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.mashup.moit.config.AuthConfig.AuthProperty
 import com.mashup.moit.security.jwt.JwtTokenSupporter
-import org.apache.commons.lang3.StringUtils
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
@@ -19,8 +18,7 @@ class AuthConfig {
     }
 
     @ConfigurationProperties(prefix = "moit.auth")
-    class AuthProperty {
-        val jwtSecretKey: String = StringUtils.EMPTY
-    }
+    data class AuthProperty(var jwtSecretKey: String)
+
 }
 
