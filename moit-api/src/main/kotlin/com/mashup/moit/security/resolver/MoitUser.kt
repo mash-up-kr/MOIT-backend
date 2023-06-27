6b87f2f78538027207dfe46a9d2dc65de813270f
KimDoubleB <tree9295@gmail.com>
2023-06-28 00:11:14 +0900

    fix: MoitUser annotation target to AnnotationTarget.VALUE_PARAMETER

--- moit-api/src/main/kotlin/com/mashup/moit/security/resolver/MoitUser.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/security/resolver/MoitUser.kt
@@ -3,6 +3,6 @@ package com.mashup.moit.security.resolver
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Retention(AnnotationRetention.RUNTIME)
-@Target
+@Target(AnnotationTarget.VALUE_PARAMETER)
 @Schema(hidden = true)
 annotation class MoitUser
