ce4d80d1cca369428ab0bdb1232c72c495ec0e4b
SeongYunKim <cauboy17@naver.com>
2023-06-30 23:24:59 +0900

    chore: rename method name

--- moit-domain/src/main/kotlin/com/mashup/moit/domain/usermoit/UserMoitRepository.kt
+++ moit-domain/src/main/kotlin/com/mashup/moit/domain/usermoit/UserMoitRepository.kt
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 interface UserMoitRepository : JpaRepository<UserMoitEntity, Long> {
     fun existsByUserIdAndMoitId(userId: Long, moitId: Long): Boolean
     fun findAllByUserId(userId: Long): List<UserMoitEntity>
-    fun findMasterByMoitIdAndRole(moitId: Long, role: UserMoitRole): UserMoitEntity?
+    fun findByMoitIdAndRole(moitId: Long, role: UserMoitRole): UserMoitEntity?
 }
