0c01b8c3f881f0a942434cadb3c146417170590c
KimDoubleB <tree9295@gmail.com>
2023-07-07 20:17:09 +0900

    test: added test for creating jwt

---
+++ moit-api/src/test/kotlin/com/mashup/moit/security/jwt/JwtTokenSupporterTest.kt
@@ -0,0 +1,34 @@
+package com.mashup.moit.security.jwt
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.mashup.moit.domain.user.UserRole
+import com.mashup.moit.security.authentication.UserInfo
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class JwtTokenSupporterTest {
+
+    @Test
+    fun createTokenTest() {
+        // given
+        val testKey = "abcd1234!@#$-=-=최고"
+        val objectMapper = ObjectMapper()
+        val jwtTokenSupporter = JwtTokenSupporter(testKey, objectMapper)
+        val userInfo = UserInfo(
+            1L,
+            "auth0|abc@naver.com",
+            "나는야 최고고",
+            1,
+            "abc@abc.com",
+            setOf(UserRole.USER)
+        )
+
+        // when
+        val token = jwtTokenSupporter.createToken(userInfo)
+
+        // then
+        println("token = ${token}")
+        assertThat(token.length).isPositive()
+    }
+
+}
