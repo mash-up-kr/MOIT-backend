ca710c8621f421e91ded8587b243e03c9c8246f3
KimDoubleB <tree9295@gmail.com>
2023-07-07 20:45:09 +0900

    feat: null check when authentication is null

--- moit-api/src/main/kotlin/com/mashup/moit/security/resolver/UserInfoArgumentResolver.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/security/resolver/UserInfoArgumentResolver.kt
@@ -1,8 +1,9 @@
 package com.mashup.moit.security.resolver
 
+import com.mashup.moit.common.exception.MoitException
+import com.mashup.moit.common.exception.MoitExceptionType
 import com.mashup.moit.security.authentication.JwtAuthentication
 import com.mashup.moit.security.authentication.UserInfo
-import jakarta.validation.ValidationException
 import org.springframework.core.MethodParameter
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.support.WebDataBinderFactory
@@ -18,9 +19,10 @@ class UserInfoArgumentResolver : HandlerMethodArgumentResolver {
     }
 
     override fun resolveArgument(parameter: MethodParameter, mavContainer: ModelAndViewContainer?, webRequest: NativeWebRequest, binderFactory: WebDataBinderFactory?): Any {
-        return when(val authentication = SecurityContextHolder.getContext().authentication) {
+        return when (val authentication = SecurityContextHolder.getContext().authentication) {
+            null -> throw MoitException.of(MoitExceptionType.INVALID_USER_AUTH_TOKEN)
             is JwtAuthentication -> authentication.principal
-            else -> throw ValidationException("The argument of GetAuth annotation is not of type UserInfo class.")
+            else -> throw MoitException.of(MoitExceptionType.INVALID_USER_AUTH_TOKEN, "The argument of GetAuth annotation is not of type UserInfo class.")
         }
     }
 
