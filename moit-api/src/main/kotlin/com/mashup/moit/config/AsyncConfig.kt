c3d6a99fd74dad93e0e72fe468c5a22a10994776
SeongYunKim <cauboy17@naver.com>
2023-07-09 13:20:37 +0900

    feat: async config

---
+++ moit-api/src/main/kotlin/com/mashup/moit/config/AsyncConfig.kt
@@ -0,0 +1,22 @@
+package com.mashup.moit.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
+
+@Configuration
+@EnableAsync
+class AsyncConfig {
+    @Bean(name = ["asyncSchedulerExecutor"])
+    fun asyncSchedulerExecutor(): Executor {
+        return ThreadPoolTaskExecutor().apply {
+            corePoolSize = 5
+            maxPoolSize = 5
+            queueCapacity = 20
+            setThreadNamePrefix("AsyncSchedulerThread-")
+            setWaitForTasksToCompleteOnShutdown(true)
+        }
+    }
+}
--- moit-api/src/main/kotlin/com/mashup/moit/scheduler/AttendanceInitializeScheduler.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/scheduler/AttendanceInitializeScheduler.kt
@@ -2,6 +2,7 @@ package com.mashup.moit.scheduler
 
 import com.mashup.moit.domain.attendance.AttendanceService
 import com.mashup.moit.domain.study.StudyService
+import org.springframework.scheduling.annotation.Async
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -13,6 +14,7 @@ class AttendanceInitializeScheduler(
     private val attendanceService: AttendanceService,
 ) {
     @Scheduled(cron = "0 */5 * * * *")
+    @Async("asyncSchedulerExecutor")
     @Transactional
     fun initializeAttendance() {
         studyService.findUnInitializedStudyStartAtBefore(LocalDateTime.now().plusMinutes(20)).forEach {
