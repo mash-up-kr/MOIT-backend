34979cd7a508d24ddc0633f22beb90e5d666b1c0
xonmin <han51361@gmail.com>
2023-07-09 17:09:55 +0900

    fix: fcm json decryption

--- moit-api/src/main/kotlin/com/mashup/moit/infra/fcm/FCMConfig.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/infra/fcm/FCMConfig.kt
@@ -4,6 +4,7 @@ import com.google.auth.oauth2.GoogleCredentials
 import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
+import org.jasypt.encryption.StringEncryptor
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -15,19 +16,25 @@ import org.springframework.core.io.ClassPathResource
 class FCMConfig(
     @Value("\${fcm.certification}")
     private val googleApplicationCredentials: String,
+    private val stringEncryptor: StringEncryptor
 ) {
     val logger: Logger = LoggerFactory.getLogger(FCMConfig::class.java)
-    
+
     @Bean
     fun firebaseMessaging(): FirebaseMessaging {
         val resource = ClassPathResource(googleApplicationCredentials)
+
+        // JSON 파일을 문자열로 읽고 필요한 값을 복호화하여 사용
+        val jsonContent = resource.inputStream.bufferedReader().use { it.readText() }
+        val decryptedJsonContent = decryptEncryptedValues(jsonContent)
+
         val firebaseAppList = FirebaseApp.getApps()
 
         val firebaseApp = if (firebaseAppList != null && firebaseAppList.isNotEmpty()) {
             firebaseAppList.find { it.name.equals(FirebaseApp.DEFAULT_APP_NAME) }
         } else {
             val options: FirebaseOptions = FirebaseOptions.builder()
-                .setCredentials(GoogleCredentials.fromStream(resource.inputStream))
+                .setCredentials(GoogleCredentials.fromStream(decryptedJsonContent.byteInputStream()))
                 .build()
 
             FirebaseApp.initializeApp(options)
@@ -35,5 +42,16 @@ class FCMConfig(
 
         logger.info("FirebaseApp initialization complete")
         return FirebaseMessaging.getInstance(firebaseApp)
+    }
+
+    private fun decryptEncryptedValues(jsonContent: String): String {
+        val regex = Regex("""ENC\((.*?)\)""")  // ENC() 패턴을 정규식으로 검색
+        val decryptedContent = regex.replace(jsonContent) { matchResult ->
+            val encryptedValue = matchResult.groupValues[1]
+            val decryptedValue = stringEncryptor.decrypt(encryptedValue)
+            decryptedValue
+        }
+
+        return decryptedContent
     }
 }
