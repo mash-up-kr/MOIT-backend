cf04cd132adf9ce40f79def5ddf7e18eaed1b09d
KimDoubleB <tree9295@gmail.com>
2023-07-19 00:56:24 +0900

    feat: added logging

--- moit-api/src/main/kotlin/com/mashup/moit/infra/event/kafka/KafkaConsumer.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/infra/event/kafka/KafkaConsumer.kt
@@ -6,6 +6,8 @@ import com.mashup.moit.infra.event.FINE_CREATE_TOPIC
 import com.mashup.moit.infra.event.FineCreateEvent
 import com.mashup.moit.infra.event.STUDY_CREATE_TOPIC
 import com.mashup.moit.infra.event.StudyCreateEvent
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
@@ -14,14 +16,17 @@ class KafkaConsumer(
     private val studyService: StudyService,
     private val fineService: FineService,
 ) {
+    private val log: Logger = LoggerFactory.getLogger(KafkaConsumer::class.java)
 
     @KafkaListener(topics = [STUDY_CREATE_TOPIC])
     fun consumeStudyCreateEvent(event: StudyCreateEvent) {
+        log.debug("consumeStudyCreateEvent called: {}", event)
         studyService.createStudies(event.moitId!!)
     }
 
     @KafkaListener(topics = [FINE_CREATE_TOPIC])
     fun consumeFineCreateEvent(event: FineCreateEvent) {
+        log.debug("consumeFineCreateEvent called: {}", event)
         fineService.create(event.attendanceId!!, event.moitId!!)
     }
 
