966ab5cc2d216df4d74255da4d0edff6fe67fedc
KimDoubleB <tree9295@gmail.com>
2023-07-19 00:42:48 +0900

    feat: added FineCreateEvent consume logic

--- moit-api/src/main/kotlin/com/mashup/moit/infra/event/kafka/KafkaConsumer.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/infra/event/kafka/KafkaConsumer.kt
@@ -1,6 +1,9 @@
 package com.mashup.moit.infra.event.kafka
 
+import com.mashup.moit.domain.fine.FineService
 import com.mashup.moit.domain.study.StudyService
+import com.mashup.moit.infra.event.FINE_CREATE_TOPIC
+import com.mashup.moit.infra.event.FineCreateEvent
 import com.mashup.moit.infra.event.STUDY_CREATE_TOPIC
 import com.mashup.moit.infra.event.StudyCreateEvent
 import org.springframework.kafka.annotation.KafkaListener
@@ -9,11 +12,17 @@ import org.springframework.stereotype.Component
 @Component
 class KafkaConsumer(
     private val studyService: StudyService,
+    private val fineService: FineService,
 ) {
-    
+
     @KafkaListener(topics = [STUDY_CREATE_TOPIC])
-    fun consumeCreateStudyEvent(event: StudyCreateEvent) {
+    fun consumeStudyCreateEvent(event: StudyCreateEvent) {
         studyService.createStudies(event.moitId!!)
     }
-    
+
+    @KafkaListener(topics = [FINE_CREATE_TOPIC])
+    fun consumeFineCreateEvent(event: FineCreateEvent) {
+        fineService.create(event.attendanceId!!, event.moitId!!)
+    }
+
 }
