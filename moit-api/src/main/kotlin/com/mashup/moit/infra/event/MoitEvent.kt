36a6e10e5527cebc3b70da40bcdd6af57aae6543
KimDoubleB <tree9295@gmail.com>
2023-07-19 00:42:38 +0900

    feat: define FineCreateEvent

--- moit-api/src/main/kotlin/com/mashup/moit/infra/event/MoitEvent.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/infra/event/MoitEvent.kt
@@ -9,3 +9,9 @@ data class StudyCreateEvent(val moitId: Long?) : MoitEvent {
         return STUDY_CREATE_TOPIC
     }
 }
+
+data class FineCreateEvent(val attendanceId: Long?, val moitId: Long?) : MoitEvent {
+    override fun getTopic(): String {
+        return FINE_CREATE_TOPIC
+    }
+}
--- moit-api/src/main/kotlin/com/mashup/moit/infra/event/MoitEventTopic.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/infra/event/MoitEventTopic.kt
@@ -1,3 +1,4 @@
 package com.mashup.moit.infra.event
 
 const val STUDY_CREATE_TOPIC = "study_create"
+const val FINE_CREATE_TOPIC = "fine_create"
