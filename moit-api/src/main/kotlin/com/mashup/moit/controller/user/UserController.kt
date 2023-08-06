e1a16054531275b240c2a2fddb3ceba5dbebb1cc
Jaemin.Park <once29@naver.com>
2023-08-06 18:53:46 +0900

    Fix updating fcm token api request body

--- moit-api/src/main/kotlin/com/mashup/moit/controller/user/UserController.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/controller/user/UserController.kt
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -35,7 +36,7 @@ class UserController(
 
     @Operation(summary = "FCM Token 갱신", description = "Update FCM Token API")
     @PostMapping
-    fun updateFcmToken(@GetAuth userInfo: UserInfo, request: UserFcmTokenRequest): MoitApiResponse<Unit> {
+    fun updateFcmToken(@GetAuth userInfo: UserInfo, @RequestBody request: UserFcmTokenRequest): MoitApiResponse<Unit> {
         userFacade.updateFcmToken(userInfo.id, request.fcmToken)
         return MoitApiResponse.success()
     }
