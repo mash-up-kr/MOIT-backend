ef2b5a7aadcebaa3b79d417108929d4f9b2d5412
KimDoubleB <tree9295@gmail.com>
2023-08-06 17:23:49 +0900

    feat: added error logging

--- moit-api/src/main/kotlin/com/mashup/moit/common/ControllerExceptionAdvice.kt
+++ moit-api/src/main/kotlin/com/mashup/moit/common/ControllerExceptionAdvice.kt
@@ -2,6 +2,8 @@ package com.mashup.moit.common
 
 import com.mashup.moit.common.exception.MoitException
 import com.mashup.moit.common.exception.MoitExceptionType
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -9,8 +11,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
 class ControllerExceptionAdvice {
+    private val log: Logger = LoggerFactory.getLogger(ControllerExceptionAdvice::class.java)
+
     @ExceptionHandler(Exception::class)
     fun handleException(exception: Exception): ResponseEntity<MoitApiResponse<Any>> {
+        log.error("Exception handler", exception)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(
                 MoitApiResponse(
@@ -26,6 +31,7 @@ class ControllerExceptionAdvice {
 
     @ExceptionHandler(MoitException::class)
     fun handleMoitException(exception: MoitException): ResponseEntity<MoitApiResponse<Any>> {
+        log.error("MoitException handler", exception)
         return ResponseEntity.status(exception.httpStatusCode)
             .body(
                 MoitApiResponse(
